eadc9fa94db69dea8eb5f8512bfe2672c15c8b7c
Zero <58788576+babyyu0@users.noreply.github.com>
2025-07-23 08:22:46 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	int basketSize = 0, inputCount = 0;
+	std::cin >> basketSize >> inputCount;
+	std::vector<int> basketNums;
+
+	// 0번째 인덱스를 tmp 변수처럼 사용한다.
+	for (int i = 0; i <= basketSize; i++) {
+		basketNums.push_back(i);
+	}
+
+	int idx1 = 0, idx2 = 0;
+	for (int i = 0; i < inputCount; i++) {
+		std::cin >> idx1 >> idx2;
+
+		if (idx1 > 0 && idx1 <= idx2 && idx2 <= basketSize) {
+			basketNums[0] = basketNums[idx1];
+			basketNums[idx1] = basketNums[idx2];
+			basketNums[idx2] = basketNums[0];
+		}
+	}
+
+	for (int i = 1; i <= basketSize; i++) {
+		std::cout << basketNums[i] << " ";
+	}
+	std::cout << std::endl;
+}
